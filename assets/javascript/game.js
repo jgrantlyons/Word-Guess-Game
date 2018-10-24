84ab3fa450a1013014211e69fd418e81733bea45
Jr <jgrantlyons@gmail.com>
2018-10-23 21:36:09 -0700

    so close...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,42 +14,71 @@ let wordChoice = words[Math.floor(Math.random() * words.length)];
 let wordChoiceIndex = words.indexOf(wordChoice);
 
 
-//display random word using dashes
+// //display random word using dashes
 let dashes = ["_"];
 for (i = 1; i < wordChoice.length; i++) {
     dashes.push("_");
 }
+
 
 //listen for keyboard input
 document.addEventListener('keydown', function (event) {
     let key = event.key;
     key = key.toString();
 
+
+
+
+
     //search random word for keyboard inputs
     let randomWord = wordChoice.split("");
     let letterIndex = randomWord.indexOf(key);
-    console.log(randomWord)
+
+    console.log(randomWord);
     console.log(letterIndex)
 
-
-    if (letterIndex > -1) {
-        //display correctly guessed letters
-        dashes[letterIndex] == wordChoice[letterIndex];
-        $("#theWord").append(dashes)
+    function refresh() {
+        guessesremaining = 5;
+        wordChoice = words[Math.floor(Math.random() * words.length)];
+        wordChoiceIndex = words.indexOf(wordChoice);
+        for (i = 1; i < wordChoice.length; i++) {
+            dashes.push("_");
+        }
     }
 
 
+    if (letterIndex > -1) {
+
+        //display correctly guessed letters
+        dashes[letterIndex] = wordChoice[letterIndex];
+        document.getElementById("wordSpot").innerText = dashes.join(" ");
+
+    }
+    // else
+    //     if (guessesRemaining < 0) {
+    //         losses = losses - 1;
+    //         document.getElementById("losses").innerText(losses);
+    //         refresh();
+    //     }
+    //     else
+    //         if (dashes = wordChoice) {
+    //             wins++;
+    //             document.getElementById("wins").innerText(wins)
+    //             refresh();
+    //         }
+            else {
+                guessesRemaining = guessesRemaining - 1
+                document.getElementById("guessesRemaining").innerText = guessesRemaining;
+            }
+
+
+
+
+
+
+
+
 });
-
-
-
-
-
-
-
-
-
-
 
 
 
